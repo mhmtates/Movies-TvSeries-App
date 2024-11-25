aca8f9730b41be8ba0303df98f642e2a6052e725
Mehmet Ateş Özateş <ozatesatess@gmail.com>
2024-11-25 04:34:02 +0300

    updated detailspage, api service and added helpers.js file

--- src/services/api.js
+++ src/services/api.js
@@ -6,9 +6,6 @@ export const imagePathOriginal = "https://image.tmdb.org/t/p/original";
 const baseUrl = "https://api.themoviedb.org/3";
 const apiKey = import.meta.env.VITE_TMDB_API_KEY;
 
-
-
-
 export const fetchTrending = async (timeWindow = "day") => {
   const { data } = await axios.get(`${baseUrl}/trending/all/${timeWindow}?api_key=${apiKey}`);
 
@@ -16,7 +13,7 @@ export const fetchTrending = async (timeWindow = "day") => {
 };
 
 export const fetchDetails = async(type,id) => {
-  const response =  await axios.get(`${baseUrl}/${type}/${id}?api_key=${apiKey}`);
+  const response =  await axios.get(`${baseUrl}/${type}/${id}?api_key=${apiKey}&language=tr`);
   return response?.data;
 };
 
