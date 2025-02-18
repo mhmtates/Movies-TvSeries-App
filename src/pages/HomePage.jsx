ae9398bab2a5e9826305c531c560a09d76965168
Mehmet Ateş Özateş <ozatesatess@gmail.com>
2025-02-18 04:24:37 +0300

    used react-slick and slick-carousel packages in homepage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,14 @@
 import { useState, useEffect } from "react";
-import { Container, Heading, Grid, Flex, Box, Skeleton } from "@chakra-ui/react";
+import { Container, Heading, Flex, Box, Skeleton, Button } from "@chakra-ui/react";
 import Cards from "../components/Cards.jsx";
 import { fetchTrending } from "../services/api.js";
+import { sliderSettings } from "../utils/helpers.js";
+import Slider from "react-slick"; 
 
 const HomePage = () => {
   const [data, setData] = useState([]);
   const [timeWindow, setTimeWindow] = useState("day");
   const [loading, setLoading] = useState(true);
-
-
 
   useEffect(() => {
     setLoading(true);
@@ -22,50 +22,51 @@ const HomePage = () => {
         setLoading(false);
       })
   }, [timeWindow]);
-  console.log(data);
 
-
+  
   return (
     <Container maxW={"container.xl"}>
+      {/* Başlık ve Butonlar */}
       <Flex alignItems={"baseline"} gap={"4"} my={"10"}>
         <Heading as={"h2"} fontSize={"md"} textTransform={"uppercase"}>
           Trend Olanlar
         </Heading>
         <Flex alignItems={"center"} gap={"2"} border={"1px solid teal"} borderRadius={"20px"}>
-          <Box as="button"
-            px="3"
-            py="1"
-            borderRadius={"20px"}
-            bg={`${timeWindow === "day" ? "gray.800" : ""}`}
-            onClick={() => setTimeWindow("day")}>Bugün</Box>
-          <Box as="button"
-            px="3"
-            py="1"
-            borderRadius={"20px"}
-            bg={`${timeWindow === "week" ? "gray.800" : ""}`}
-            onClick={() => setTimeWindow("week")}>Bu Hafta</Box>
+          <Button 
+            size="sm"
+            variant={timeWindow === "day" ? "solid" : "outline"}
+            colorScheme="teal"
+            onClick={() => setTimeWindow("day")}
+          >
+            Bugün
+          </Button>
+          <Button 
+            size="sm"
+            variant={timeWindow === "week" ? "solid" : "outline"}
+            colorScheme="teal"
+            onClick={() => setTimeWindow("week")}
+          >
+            Bu Hafta
+          </Button>
         </Flex>
       </Flex>
 
-      <Grid
-        templateColumns={{
-          base: "1fr",
-          sm: "repeat(2, 1fr)",
-          md: "repeat(4, 1fr)",
-          lg: "repeat(5, 1fr)",
-        }}
-        gap={"4"}
-      >
-        {data && data?.map((item, i) =>
-          loading ? (
-            <Skeleton height={300} key={i} />
-          ) : (
-            <Cards key={item?.id} item={item} type={item?.media_type} />
-          )
-        )}
-
-      </Grid>
-
+      {/* Slider */}
+      {loading ? (
+        <Flex gap="4">
+          {[...Array(5)].map((_, i) => (
+            <Skeleton height="300px" width="200px" key={i} />
+          ))}
+        </Flex>
+      ) : (
+        <Slider {...sliderSettings}>
+          {data?.map((item) => (
+            <Box key={item?.id} p="2">
+              <Cards item={item} type={item?.media_type} />
+            </Box>
+          ))}
+        </Slider>
+      )}
     </Container>
   );
 };
